857f3a0d8364c0b1a6541429d7d8bd02b22a7cd2
jman45-9 <jcope0787@gmail.com>
2023-08-11 21:31:52 -0600

    added bad card error handling for splitFrontBack

--- main.cpp
+++ main.cpp
@@ -11,14 +11,45 @@ FlashCard *readSet(char *filename)
 {
     int cardsNum = countFileLines(filename);
 
-    std::ifstream setFile{filename};
-    if (!setFile) 
+    std::ifstream fileForSet{filename};
+    if (!fileForSet) 
     {
         std::cerr << "Failed to open set!\n";
         return NULL;
     }
 
+/* Saving for later
+    FlashCard *setArray = (FlashCard *)(malloc(sizeof(FlashCard) * cardsNum));
+    for (int iii = 0; cardsNum > iii; iii++)
+    {
+        string frontText;
+        string backText;
+        string cardInfo;
+
+        std::getline(fileForSet, cardInfo);
+        spiltFrontBack();
+    }
+    */
 }
+
+void spiltFrontBack(string cardInfo, string *frontText, string *backText)
+{
+    int splitMarker = -1;
+    for(int iii = 0; cardInfo.length() >= iii; iii++)
+    {
+        if (cardInfo[iii] == ':' && cardInfo[iii+1] == '::')
+        {
+            
+        }
+    }
+
+    if (splitMarker == -1)
+    {
+        std::cerr << "Please add a '::' between the front and back of" << cardInfo << '\n';
+        exit(1);
+    }
+}
+
 
 int countFileLines(char *filename)
 {
