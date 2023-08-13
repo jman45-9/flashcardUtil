70787f33b96205eb8ebd08567abfdd873ba0a16d
jman45-9 <jcope0787@gmail.com>
2023-08-12 20:57:17 -0600

    added constuctor and attributes

--- include/FlashcardSet.hpp
+++ include/FlashcardSet.hpp
@@ -1,12 +1,17 @@
 #ifndef FLASHCARD_FLASHCARD_SET_H_
 #define FLASHCARD_FLASHCARD_SET_H_
 
+#include <string>
+
 #include "structs.hpp"
 
 class FlashcardSet 
 {
     public:
+        FlashCard *setArray;
+        int numCards;
 
+    FlashcardSet(int numCards, FlashCard *setArray);
 };
 
 #endif // FLASHCARD_FLASHCARD_SET_H_
--- src/FlashcardSet.cpp
+++ src/FlashcardSet.cpp
@@ -1,2 +1,7 @@
 #include "../include/FlashcardSet.hpp"
 
+FlashcardSet::FlashcardSet(int numCards, FlashCard *setArray)
+{
+    this->setArray = setArray;
+    this->numCards = numCards;
+}
