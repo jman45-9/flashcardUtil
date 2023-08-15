649db383c113376520c418814dd4ddca157b2eec
Jeremy Cope <jeremy.cope7142@gmail.com>
2023-08-15 10:39:51 -0700

    fixed triple colon in printcards

--- src/FlashcardSet.cpp
+++ src/FlashcardSet.cpp
@@ -14,7 +14,7 @@ FlashcardSet::~FlashcardSet()
 bool FlashcardSet::printCards()
 {
     for(int iii = 0; this->numCards > iii; iii++)
-        std::cout << iii << ") " << ((this->setArray)+iii)->front << ":::" << 
+        std::cout << iii << ") " << ((this->setArray)+iii)->front << "::" << 
         ((this->setArray)+iii)->back << "\n";
     Logger::logString("user printed cards");
     return CONTINUE;
