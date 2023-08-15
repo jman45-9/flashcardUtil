4cf4003a9d6fa8512b01b45cbc48568479d5f246
jman45-9 <jcope0787@gmail.com>
2023-08-14 21:29:52 -0600

    added org headers

---
+++ include/classHeaders.hpp
@@ -0,0 +1,6 @@
+#ifndef FLASHCARD_CLASS_HEADERS_H_
+#define FLASHCARD_CLASS_HEADERS_H_
+
+#include "FlashcardSet.hpp";
+
+#endif //FLASHCARD_CLASS_HEADERS_H_
---
+++ include/macrosStdLibs.hpp
@@ -0,0 +1,13 @@
+#ifndef FLASHCARD_MACROS_STDLIBS_H_
+#define FLASHCARD_MACROS_STDLIBS_H_
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cassert>
+
+#define CONTINUE 1
+#define QUIT 0
+
+#endif //FLASHCARD_MACROS_STDLIBS_H_
